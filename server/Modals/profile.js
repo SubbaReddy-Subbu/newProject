08a5b0c16d1904c10961f87ae286ec7af124896b
SubbareSubbaReddy-Subbu <subbareddydaginti406@gmail.com>
2023-12-04 22:14:50 +0530

    crud initialized

---
+++ server/Modals/profile.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const autoIncrement = require('mongoose-auto-increment');
+
+
+const profileSchema = new Schema({
+    Profile_Name: {
+        type: String,
+        required: true
+    },
+    Profile_img: {
+        type: String,
+    },
+    Email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    Mobile_No: {
+        type: Number,
+        required: true
+    },
+    Gender: {
+        type: String,
+        required: true
+    }
+});
+// Add auto-increment plugin
+// autoIncrement.initialize(mongoose.connection);
+// profileSchema.plugin(autoIncrement.plugin, {
+//     model: 'Profile',
+//     field: 'Id', // This assumes you want to use 'Id' as the auto-incremented field
+//     startAt: 1,
+//     incrementBy: 1
+// });
+
+
+module.exports = mongoose.model("Profile",profileSchema);
--- server/server.js
+++ server/server.js
@@ -1,32 +1,73 @@
 const express  =require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser")
+
 const userModel = require("./Modals/User");
+const ProfileModal = require("./Modals/profile")
 const app =express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/newProject").then(console.log("db connected")).catch((err)=>(err))
+mongoose.connect("mongodb+srv://subbareddydaginti406:subbareddy@cluster0.osu1rar.mongodb.net/profile?retryWrites=true&w=majority").then(console.log("db connected")).catch((err)=>(err))
 
-app.post("/login",(req,res)=>{
-    const {email,password} = req.body;
-    userModel.findOne({email: email}).then(user=>{
-        if(user){
-            if(user.password === password){
+// {app.post("/login",(req,res)=>{
+//     const {email,password} = req.body;
+//     userModel.findOne({email: email}).then(user=>{
+//         if(user){
+//             if(user.password === password){
                 
-            } else{res.json("The password has incorrect")}
-        }else{
-            res.json("There is no User with this Detailes")
-        }
-    })
+//             } else{res.json("The password has incorrect")}
+//         }else{
+//             res.json("There is no User with this Detailes")
+//         }
+//     })
+// })}
+
+
+// {app.post("/register",(req,res)=>{
+//      userModel.create(req.body)
+//     .then(Users => res.json(Users))
+//     .catch((err)=>res.json(err))
+// })}
+
+app.post('/Profile',(req,res)=>{
+    ProfileModal.create(req.body)
+    .then(profile => res.json(profile))
+    .catch((err)=>res.json(err))
+   
 })
 
-app.post("/register",(req,res)=>{
-     userModel.create(req.body)
-    .then(Users => res.json(Users))
-    .catch((err)=>res.json(err))
+app.delete("/deleteall",(req,res)=>{
+    ProfileModal.deleteMany()
+    return res.send('Success fully Delete')
 })
+app.delete("/delete/:id", async (req, res) => {
+    try {
+      const deletedProfile = await ProfileModal.findByIdAndDelete(req.params.id);
+  
+      if (!deletedProfile) {
+        return res.status(404).json({ message: 'Profile not found' });
+      }
+  
+      return res.status(200).json({ message: 'Successfully deleted profile', deletedProfile });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  })
+
+app.get('/getProfile',async(req,res)=>{
+    try{
+        return res.json(await ProfileModal.find());
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
 
 app.listen(3001,()=>{
     console.log("server runing on Port no 3001")
